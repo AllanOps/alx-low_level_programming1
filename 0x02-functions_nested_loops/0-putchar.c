882328249757ca359aea80d2a3ef444aceb5e5fd
AllanOps <ombongiallan@gmail.com>
2022-03-17 02:40:43 +0300

    Fixed issue printing putchar on a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
-/**
- * _putchar - writes the char c to stdout
- * @c: the character to print
- * *Return: on success 1
- * On error, -1 is returned, and errorNumber is set accordingly
- */
- int _putchar(char c)
- {
-        return(write(1, &c, 1));
- }
- int main(void)
- {
-        _putchar();
-        return(0);
- }
+#include "main.h"
 
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
