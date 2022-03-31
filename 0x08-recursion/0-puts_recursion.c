354e64a7ab01e8db28ac69e93c3470c54e3db1a4
AllanOps <ombongiallan@gmail.com>
2022-03-31 15:15:12 +0300

    Hot fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
  * On error, -1 is returned, and err no is set appropriately.
  */
 
-void _puts_recursion(char s*s)
+void _puts_recursion(char *s)
 {
 	if (*s == 0)
-{
-			_putchar('\n');
-			return;
-		}
+	{
+		_putchar('\n');
+		return;
+	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
