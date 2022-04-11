0e30a39a3422690f26e72a9ddbbaf85523965748
AllanOps <ombongiallan@gmail.com>
2022-04-11 19:24:01 +0300

    Printing the name of the filethat it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define FILENAME __FILE__
+
+/**
+ * main - prints the filename of where its been compiled from
+ *
+ * Return value always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", FILENAME);
+
+	return 0;
+}
